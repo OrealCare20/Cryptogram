f1574b7ffdbb43de247f357a7c7ce4004461c10c
OrealCare20 <orealcare20@gmail.com>
2024-12-30 12:30:38 +0500

    Update .gitignore to exclude APK files and build outputs

--- App.tsx
+++ App.tsx
@@ -1,118 +1,130 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+import { View, Text, StyleSheet, Dimensions, Image, ImageBackground, TouchableOpacity } from 'react-native';
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import OutlinedText from '@kdn0325/react-native-outlined-text';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const { width, height } = Dimensions.get('screen');
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const CALENDER_WIDTH = width / 2 - 100;
+const CALENDER_RATIO = CALENDER_WIDTH / 288;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const BRAIN_WIDTH = width / 2;
+const BRAIN_RATIO = BRAIN_WIDTH / 663;
+
+const START_BUTTON = width / 2;
+const START_BUTTON_RATIO = START_BUTTON / 353;
+
+const App = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <View style={style.container}>
+      <View style={{ width: '100%', flexDirection: 'row', alignItems: 'center' }}>
+        <Image style={style.headerBadgeIcon} source={require('./assets/images/OBJECTS.png')} />
+        <View style={style.headerBadge}>
+          <Text style={style.headerText}>Full</Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+        <Image style={{ width: 30, height: 30, marginLeft: 4 }} source={require('./assets/images/plus.png')} />
+      </View>
+
+      <View style={style.levelContainer}>
+        <OutlinedText
+          text={'Daily Challenge'}
+          fontSize={30}
+          fontWeight={'500'}
+          outlineColor={'#000'}
+          shadowLine={2}
+          fontFamily='Roboto-BlackItalic'
+          customStyle={{ marginVertical: 20 }}
+        />
+
+        <ImageBackground style={style.calender} source={require('./assets/images/calender.png')}>
+          <OutlinedText
+            text={'15'}
+            fontSize={18}
+            outlineColor={'#000'}
+            shadowLine={2}
+            fontFamily='Roboto-BlackItalic'
+            customStyle={{ marginTop: 15, textAlign: 'center' }}
+          />
+          <OutlinedText
+            text={'Level'}
+            fontSize={18}
+            outlineColor={'#000'}
+            shadowLine={2}
+            fontFamily='Roboto-BlackItalic'
+          />
+        </ImageBackground>
+
+        <View style={{ width: '70%' }}>
+          <OutlinedText
+            text={'Complete 15 Level to Unlock'}
+            fontSize={22}
+            outlineColor={'#000'}
+            color='#FFB002'
+            shadowLine={2}
+            fontFamily='Roboto-BlackItalic'
+            customStyle={{ textAlign: 'center', alignSelf: 'center', marginBottom: 25 }}
+          />
+        </View>
+      </View>
+
+      <Image style={style.brain} source={require('./assets/images/brain.png')} />
+      <TouchableOpacity>
+        <ImageBackground style={style.startButton} source={require('./assets/images/button.png')}>
+          <Image source={require('./assets/images/play-icon.png')} style={{ width: 21, height: 24.6, left: '29%' }} />
+          <OutlinedText
+            text={'Start'}
+            fontSize={30}
+            fontWeight={'500'}
+            outlineColor={'#000'}
+            shadowLine={2}
+            fontFamily='Roboto-BlackItalic'
+          />
+        </ImageBackground>
+      </TouchableOpacity>
+    </View>
+  )
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: `rgba(255, 255, 255, 1)`,
+    padding: 10
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  headerBadge: { width: 53, height: 32, backgroundColor: '#E7D2C6', borderRadius: 7, justifyContent: 'center', alignItems: 'flex-end', paddingEnd: 10, marginLeft: '5%' },
+  headerText: { color: '#BE8763', fontSize: 12, fontWeight: '600' },
+  headerBadgeIcon: {
+    width: 34,
+    height: 34,
+    position: 'absolute',
+    zIndex: 1
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  levelContainer: {
+    backgroundColor: '#EBDFD7',
+    width: width * 0.60,
+    alignSelf: 'center',
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    marginTop: 50
   },
-  highlight: {
-    fontWeight: '700',
+  calender: {
+    width: CALENDER_WIDTH,
+    height: 312 * CALENDER_RATIO,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
+  brain: {
+    width: BRAIN_WIDTH,
+    height: 477 * BRAIN_RATIO,
+    alignSelf: 'center',
+    marginVertical: 70
+  },
+  startButton: {
+    width: START_BUTTON,
+    height: 133 * START_BUTTON_RATIO,
+    flexDirection: 'row',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
-
 export default App;
